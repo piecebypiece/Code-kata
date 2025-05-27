d03c4e0f24bd0fcd98aa72e7f9b45eb177e64346
piecebypiece <chlx357@gmail.com>
2025-05-27 10:51:07 +0900

    0527 Kata 짝수의 합

---
+++ CodeKATA.cpp
@@ -0,0 +1,12 @@
+// CodeKATA.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "KATA_0527.hpp"
+
+int main()
+{
+	std::cout << "Hello World!\n";
+	int evenSum = KATA_0527::solution(10); // https://school.programmers.co.kr/learn/courses/30/lessons/120831
+	std::cout << "evenSum = " << evenSum << "\n";
+}
